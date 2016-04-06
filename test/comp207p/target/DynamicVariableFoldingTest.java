191e358fda458c4bf4e4833c2cd8f41e25e10a0c
Istvan Hoffer <hofferistvan@me.com>
2016-04-06 14:30:09 +0200

    beacuse we love git

--- test/comp207p/target/DynamicVariableFoldingTest.java
+++ test/comp207p/target/DynamicVariableFoldingTest.java
@@ -30,7 +30,7 @@ public class DynamicVariableFoldingTest
         System.setOut(null);
     }
 
-    /* @Test
+    /*@Test
     public void testMethodOne()
     {
         assertEquals(1301, dvf.methodOne());
@@ -53,7 +53,7 @@ public class DynamicVariableFoldingTest
     public void testMethodThree()
     {
         assertEquals(84, dvf.methodThree());
-    } */
+    }*/
     
     @Test
     public void testMethodFour(){
